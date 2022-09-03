09a37713929116d53178411e4bf52f90f9b31e59
subrotomojumder <97802263+subrotomojumder@users.noreply.github.com>
2022-09-03 10:57:42 +0600

    dynamic news item seting finish

--- js/main.js
+++ js/main.js
@@ -9,48 +9,56 @@ const showCategory = async () => {
     const categoryList = document.getElementById('category-list');
     menu.forEach( category => {
         const div = document.createElement('div');
-        div.innerHTML = `<div onclick="displayNews('${category.category_id}')" class="category">${category.category_name}</div>`;
+        div.innerHTML = `<div onclick="displayNews('${category.category_id}','${category.category_name}')" class="category fw-semibold">${category.category_name}</div>`;
         categoryList.appendChild(div);
     });
 }
 showCategory()
 
-const displayNews = async id => {
-    const res = await fetch(` https://openapi.programming-hero.com/api/news/category/${id}`);
+const displayNews = async (id, cateName) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${id}`);
     const data = await res.json();
     const newsBlogs = data.data;
+    // count 1 category news
+    const countNews = document.getElementById('count-news');
+    countNews.innerHTML = `Event: Found <span class="text-warning">${newsBlogs.length}</span> items in <span class="text-success">${cateName}</span> category`;
     const newsContainer = document.getElementById('news-container');
     newsContainer.innerHTML = '';
     newsBlogs.forEach( blog => {
-        const {author, details, thumbnail_url, total_view, title, _id} = blog;
+        const {author, details, thumbnail_url, total_view, title, others_info,_id} = blog;
         const div = document.createElement('div');
         div.classList.add('col')
         div.innerHTML = `
-            <div class="row g-3 my-4 p-2 bg-white rounded-3">
+            <div class="row g-3 my-4 p-4 bg-white rounded-3">
                 <div class="col-11 col-sm-3 text-center">
-                <img src="${thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                    <img src="${thumbnail_url}" class="img-fluid rounded-start" alt="...">
                 </div>
                 <div class="col-12 col-sm-9">
                     <div class="card-body">
-                        <h5 class="card-title fw-bold">${title}</h5>
-                        <p class="card-text">${details}</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                        <div class="d-flex justify-content-between">
-                        <div class="d-flex align-items-center">
-                            <img class="rounded-circle" src="${author ? author.img : 'unknown'}" style="width: 60px" alt="">
-                            <div>
-                                <p class ="fw-bold mb-0">${author?author.name : 'unknown'}</p>
-                                Published: ${author?author.published_date : 'unknown'}
+                        <h4 class="card-title fw-bold text-warning my-2">${title}</h4>
+                        <p class="card-text"><small class="text-muted">${others_info.is_trending ? 'Published there <span class ="text-danger fw-bold">3 days ago</span> !' : 'Published there 5 days ago!'}</small></p>
+                        <p class="card-text">${details.length > 500 ? details.slice(0, 500)+'....' : details}</p>
+                        <div class="d-flex justify-content-between me-4">
+                            <div class="d-flex align-items-center">
+                                <img class="rounded-circle" src="${author ? author.img : 'unknown'}" style="width: 60px" alt="">
+                                <div>
+                                    <p class ="fw-bold mb-0">${author?author.name : 'unknown'}</p>
+                                    Published: ${author?author.published_date : 'unknown'}
+                                </div>
                             </div>
+                            <div class=""><i class="fa-solid fa-eye"></i> ${total_view ? total_view : 'Hide'} view</div>
+                            <button onclick="displayDetails('${_id}')" class="px-3 fw-bold pt-1 btn bg-info text-light rounded-2">See more <i class="fa-sharp fa-solid fa-arrow-right-long"></i></button>
                         </div>
-                        <div class="">Views-${total_view}</div>
-                        <dib class="px-3 py-2 h3 bg-info rounded-2">&#8680;</div>
-                    </div>
                     </div>
                 </div>
             </div>
         `;
         newsContainer.appendChild(div);
-        console.log(blog)
+        // console.log(blog)
     });
+};
+
+const displayDetails = async detailsId => {
+    const res = await fetch(``)
+    console.log(detailsId)
 }
---
+++ link.js
@@ -0,0 +1,3 @@
+// https://youtu.be/rMTaPPqK2xY
+const details =  "Tucker Carlson has rarely met a dictator's ass he didn't want to kiss, and Vladimir Putin is at the very top of that puckering up list. The Fox News host is something of a folk hero in Russia: Because of the pro-Putin propaganda he so often spews, the Kremlin has encouraged Russia's state TV to air as much of Tucker's face as possible — and they're certain to love his latest rant about how Putin is winning the war in Ukraine, which does not seem to be the case. But on Fox News, what Tucker says goes."
+console.log(details.slice(0,100))
